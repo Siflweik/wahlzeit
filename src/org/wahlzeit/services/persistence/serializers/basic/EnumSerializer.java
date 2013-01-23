9bba70f31ccb09a2f2f3f3e5cf12f5efb8a4f2da
Flo <florian.weikert@gmx.de>
2013-01-23 20:57:36 +0100

    fixed grammar

--- src/org/wahlzeit/services/persistence/serializers/basic/EnumSerializer.java
+++ src/org/wahlzeit/services/persistence/serializers/basic/EnumSerializer.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import org.wahlzeit.services.persistence.Serializer;
 import org.wahlzeit.utils.EnumValue;
 
-// If we had made the assumption that all subclasses of EnumValue had the same static conversion method, we would not need this class
+// If we made the assumption that all subclasses of EnumValue had the same static conversion method, we would not need this class
 public abstract class EnumSerializer<E extends EnumValue> extends Serializer<E, Object> {
 	protected final IntegerSerializer intSerializer = new IntegerSerializer();
 	
